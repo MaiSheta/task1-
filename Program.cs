7e6e8618ca0af0c7ced0f5a53504c4eb8127e79f
MaiSheta <120672459+MaiSheta@users.noreply.github.com>
2022-12-30 23:01:36 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n1, n2, a, b, i;
+            int n1, n2,i,j,flag;
             Console.WriteLine("please enter n1");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("please enter n2");
             n2 = int.Parse(Console.ReadLine());
             for (i = n1; i <= n2; i++)
             {
-                a = 1;
-                b = 0;
-                while (i > a)
+                for (j = 2; j <= i / 2; i++)
                 {
-                    if (i % a == 0)
-                        b = b + a;
-                    a++;
+                    if (i % j == 0)
+                    {
+                        Flag = 0;
+                        break;
+                    }
                 }
-                if (b == i)
-                    Console.WriteLine("{0}", i);
-
             }
     }
 }
-
-
-
-
-    
